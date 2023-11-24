9bb42fd9f10b8a3c15e8743dba25576c0783f637
renatho <renatho@pop-os.localdomain>
2023-11-24 15:12:15 -0300

    chamando as rotas ainda nao criadas no app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const routerProducts = require('./routes/products.routes');
+const routerSales = require('./routes/sales.routes');
+
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
 });
+
+app.use('/products', routerProducts);
+app.use('/sales', routerSales);
 
 module.exports = app;
