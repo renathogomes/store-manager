3018aa4bef608fa1454111d41a7de23d4f03f268
renatho <renatho@pop-os.localdomain>
2023-11-24 16:02:14 -0300

    criando diretorios e arquivos para testes, arquivo productMock

---
+++ backend/src/tests/mock/products.mock.js
@@ -0,0 +1,30 @@
+const productsMock = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+  {
+    id: 2,
+    name: 'Traje de encolhimento',
+  },
+  {
+    id: 3,
+    name: 'Escudo do CapitÃ£o AmÃ©rica',
+  },
+];
+  
+const productByIdMock = [
+  {
+    id: 1,
+    name: 'Martelo de Thor',
+  },
+];
+  
+const resgisterProductMock = { status: 201,
+  data: { id: 4, name: 'Manopla do Thanos' } };
+  
+module.exports = {
+  productsMock,
+  productByIdMock,
+  resgisterProductMock,
+};
