1110e0a20cef81bb396e5e05f4734e0737326230
renatho <renatho@pop-os.localdomain>
2023-11-27 08:11:05 -0300

    testes da camada model

---
+++ backend/src/tests/unit/model/products.model.test.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const sinon = require('sinon');
+
+const { expect } = chai;
+
+const connection = require('../../../models/connection');
+const productsModel = require('../../../models/products.model');
+const productsMock = require('../../mock/products.mock');
+
+describe('Testa o model de produtos', function () {
+  describe('Testa o model de produtos, quando a conexão é bem sucedida', function () {
+    before(function () {
+      sinon.stub(connection, 'execute')
+        .resolves([productsMock.productsMock]);
+    });
+    
+    after(function () {
+      connection.execute.restore();
+    });
+    
+    it('Retorna um array', async function () {
+      const response = await productsModel.getAll();
+      expect(response).to.be.an('array');
+    });
+    
+    it('Retorna um array de objetos', async function () {
+      const response = await productsModel.getAll();
+      expect(response[0]).to.be.an('object');
+    });
+    
+    it('Retorna um array de objetos com as chaves "id" e "name"', async function () {
+      const response = await productsModel.getAll();
+      expect(response[0]).to.have.all.keys('id', 'name');
+    });
+  }); 
+});
