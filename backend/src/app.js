718e2c20e51c693d6f78afcff7804ccdf9bd7050
renatho <renatho@pop-os.localdomain>
2023-11-28 13:43:15 -0300

    adicionando express.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,8 @@ const { productRoutes, salesRoutes } = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
