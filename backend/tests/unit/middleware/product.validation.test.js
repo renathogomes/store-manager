ec979a35bd47f85b4d29a8a750abc22cb849534f
renatho <renatho@pop-os.localdomain>
2023-11-30 14:46:27 -0300

    testes para middleware

---
+++ backend/tests/unit/middleware/product.validation.test.js
@@ -0,0 +1,39 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { createProductValidation } = require('../../../src/middlewares/createProductValidation');
+
+describe('createProductValidation', function () {
+  it('should return 400 if name is not provided', function () {
+    const req = {
+      body: {},
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    const next = sinon.stub();
+
+    createProductValidation(req, res, next);
+
+    expect(res.status.calledWith(400)).to.equal(true);
+    expect(res.json.calledWith({ message: '"name" is required' })).to.equal(true);
+    expect(next.called).to.equal(false);
+  });
+
+  it('should return 422 if name length is less than 5', function () {
+    const req = {
+      body: { name: 'abc' },
+    };
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+    const next = sinon.stub();
+
+    createProductValidation(req, res, next);
+
+    expect(res.status.calledWith(422)).to.equal(true);
+    expect(res.json.calledWith({ message: '"name" length must be at least 5 characters long' })).to.equal(true);
+    expect(next.called).to.equal(false);
+  });
+});
