9e4dd90cc8a3fba2a78204f4dc4fab61722c1f01
renatho <renatho@pop-os.localdomain>
2023-12-01 10:20:12 -0300

    resolvido problema de coneccao colocando a hof dentro do if

--- backend/src/middlewares/createSalesValidation.js
+++ backend/src/middlewares/createSalesValidation.js
@@ -1,12 +1,16 @@
 const productIdSalesValidation = (req, res, next) => {
   const { body } = req;
+
+  if (body.some((product) => !product.productId)) {
+    return res.status(400).json({ message: '"productId" is required' });
+  }
   
-  body.every((product) => {
-    if (!product.productId) {
-      return res.status(400).json({ message: '"productId" is required' });
-    }
-    return true;
-  });
+  // body.every((product) => {
+  //   if (!product.productId) {
+  //     return res.status(400).json({ message: '"productId" is required' });
+  //   }
+  //   return true;
+  // });
 
   next();
 };
@@ -14,24 +18,31 @@ const productIdSalesValidation = (req, res, next) => {
 const quantitySalesValidation = (req, res, next) => {
   const { body } = req;
 
-  body.every((product) => {
-    if (!product.quantity && product.quantity !== 0) {
-      return res.status(400).json({ message: '"quantity" is required' });
-    }
-    return true;
-  });
+  if (body.some((product) => !product.quantity && product.quantity !== 0)) {
+    return res.status(400).json({ message: '"quantity" is required' });
+  }
+
+  // body.every((product) => {
+  //   if (!product.quantity && product.quantity !== 0) {
+  //   }
+  //   return true;
+  // });
   next();
 };
 
 const quantitySalesValidation2 = (req, res, next) => {
   const { body } = req;
+
+  if (body.some((product) => Number(product.quantity) <= 0)) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
   
-  body.every((product) => {
-    if ((Number(product.quantity) <= 0)) {
-      return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
-    }
-    return true;
-  });
+  // body.every((product) => {
+  //   if ((Number(product.quantity) <= 0)) {
+  //     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  //   }
+  //   return true;
+  // });
   next();
 };
 
